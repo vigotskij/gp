b44796bcf308194193bec417fcc02ea80fd6cc1e
vigotskij <mdqulto@gmail.com>
2017-04-28 10:58:06 -0300

    microiteration 1.1

--- main.cpp
+++ main.cpp
@@ -1,82 +1,48 @@
 #include <iostream>
 
     /*
-     * microiteración 0.1 begin
-     *  resolvemos: #plain "Word" tipo desconocido
+     * microiteración 1.1 begin
+     *  abordamos la definición de "isNull"
      *
      *  justificación:
-     *      "Resolucion de problemas"
-     *      "Abstracción", mapeo de abstracciones entre
-     *          espacio del problema -> espacio de la solución, gap semántico
-     *      "Abstracción" abstracción de objeto(cosa) -> sustantivo -> Tipo, instancia
-     *      "Lenguaje de programación C++", 4.1 , 5.7.
+     *      "Lenguaje de programación C++", 4.1.1, 4.3, 5.2.
      *
      */
 
 struct Word
 {
-    /* microiteration 0.2, begin.
-     * resolvemos "No member named 'isNull' in 'Word'"
-     *
-     * Justificacion:
-     * "Abstracción", abstracción de comportamiento -> verbo -> funcion
-     *
-     */
+
     bool isNull( void )
     {
-        bool result = true ;
-        // to define...
-        return result ;
+        return items[ 0 ] == '\0' ;
     }
-    // microiteration 0.2, end
 };
 
-    //microiteración 0.1 end.
+
+Word getWordFromText( void )
+{
+    Word word ;
+    // to define...
+    return word ;
+}
+
+void process ( const Word& word )
+{
+    // to define...
+}
 
 int main( int argc , const char * argv[] )
 {
-    /*
-     * Dado un texto que termina con un punto. Construir una lista de palabras
-     * y su frecuencia (veces que la palabra aparece en el texto).
-     * El texto se procesa caracter a caracter.
-     *
-     * approach #1, un planteo posible...
-     *      1.- mientras haya una palabra en el texto.
-     *      2.- obtener la palabra
-     *      3.- procesar la palabra
-     *
-     * justificación,
-     *      "resolución de problemas".
-     *      (Blum&Niss, Comfrey, Polya y otros)
-     *
-     * luego ;
-     *      Sea palabra una Palabra ;
-     *      palabra <- obtenerUnaPalabraDelTexto ;
-     *      mientras ( ( palabra no sea la palabra nula ) )
-     *      // (palabra nula => ausencia de palabra )
-     *      {
-     *          procesar( palabra ) ;
-     *          palabra <- obtenerUnaPalabraDelTexto ;
-     *      }
-     *
-     *
-     */
+    unsigned count = 0 ;
     Word word ;
     word = getWordFromText() ;
     while ( !( word.isNull() ) )
     {
         process( word ) ;
+        count++ ;
         word = getWordFromText() ;
     }
-    /*
-     * Compiler semantic issue...
-     *  issue: a matter that is in dispute between two or
-     *      more parties (Merriam-Webster).
-     *  semantic: of or relating to the meanings of words
-     *      and phrases (Merriam-Webster).
-     *
-     *  ~= controversia relativa al significado de algunas palabras.
-     *    (entre el compilador y el autor del código.
-     */
+    std::cout << "words in text: " << count << std::endl ;
+    // microiteration 1.1, end.
     return 0;
 }
